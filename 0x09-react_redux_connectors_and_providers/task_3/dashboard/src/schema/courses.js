e6277c5ea27b8d511b92c9ffc9cdc382065ea611
yego5 <shachz.github@gmail.com>
2024-12-14 01:11:46 +0300

    react 009

---
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/schema/courses.js
@@ -0,0 +1,21 @@
+import { normalize, schema } from "normalizr";
+
+const courses = new schema.Entity("courses");
+
+const coursesNormalizer = (data) => {
+  const normalizedData = normalize(data, [courses]);
+
+  return normalizedData.entities.courses;
+};
+
+export default coursesNormalizer;import { normalize, schema } from "normalizr";
+
+const courses = new schema.Entity("courses");
+
+const coursesNormalizer = (data) => {
+  const normalizedData = normalize(data, [courses]);
+
+  return normalizedData.entities.courses;
+};
+
+export default coursesNormalizer;
